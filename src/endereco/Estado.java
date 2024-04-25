3edd8a4d99e1ebcfca5c3fa33754f208d92aae5c
arthu <arthurfpeixoto@gmail.com>
2024-04-25 18:58:41 -0300

    class Estado

---
+++ src/endereco/Estado.java
@@ -0,0 +1,10 @@
+package endereco;
+
+import java.util.List;
+
+public class Estado {
+    private String sigla;
+    public String nome;
+    private Pais pais;
+    private List<Cidade> cidades;
+}
